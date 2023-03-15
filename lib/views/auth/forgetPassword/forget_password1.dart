ec4a410011f853d3cd3baa4b9f3206fbe2737e4e
Yana DEREVIANKO <79232031+yanaderevianko726@users.noreply.github.com>
2023-03-16 04:10:39 +0700

    add forget password

---
+++ lib/views/auth/forgetPassword/forget_password1.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+class ForgetPassword1Page extends StatefulWidget {
+  const ForgetPassword1Page({Key? key}) : super(key: key);
+
+  @override
+  State<ForgetPassword1Page> createState() => _ForgetPassword1PageState();
+}
+
+class _ForgetPassword1PageState extends State<ForgetPassword1Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Container(),
+    );
+  }
+}
---
+++ lib/views/auth/forgetPassword/forget_password2.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ForgetPassword2Page extends StatefulWidget {
+  const ForgetPassword2Page({Key? key}) : super(key: key);
+
+  @override
+  State<ForgetPassword2Page> createState() => _ForgetPassword2PagePageState();
+}
+
+class _ForgetPassword2PagePageState extends State<ForgetPassword2Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Container(),
+    );
+  }
+}
